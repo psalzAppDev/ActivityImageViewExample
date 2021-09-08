2a3a74da0a518df994fe54d2c3c5c04a31e5faa3
Peter Salz <psalz.app.dev@web.de>
2021-09-08 16:22:44 +0200

    Initial Commit

---
+++ ActivityImageViewExample/ActivityImageViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityImageViewExampleApp.swift
+//  ActivityImageViewExample
+//
+//  Created by Peter Salz on 08.09.21.
+//
+
+import SwiftUI
+
+@main
+struct ActivityImageViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityImageViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActivityImageViewExample
+//
+//  Created by Peter Salz on 08.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
